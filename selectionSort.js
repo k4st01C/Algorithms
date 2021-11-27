953792250fc04f4fb4f574341d1953165f1e8c49
Kemal Aydık <kemalaydik@gmail.com>
2021-11-27 15:07:03 +0300

    selection sort release

---
+++ selectionSort.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function swap(arr, i, j) {
+	[arr[i], arr[j]] = [arr[j], arr[i]];
+}
+
+function selectionSort(arr) {
+	let minIdx = 0;
+	let swapFlag = true;
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = i + 1; j < arr.length; j++) {
+			minIdx = arr[j] < arr[minIdx] ? j : minIdx;
+		}
+		swap(arr, minIdx, i);
+	}
+	return arr;
+}
+
+console.log(selectionSort([-10, 3, -12, 2, 10, 3, 1, -1, -5]));
